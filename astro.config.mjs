e479a08b06c29e0393998ed658bb581c640f92a5
mengdeke <somethingdk@163.com>
2026-04-21 17:16:35 +0800

    fix: base url trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://sth-dk.github.io',
-  base: '/ai-blog',
+  base: '/ai-blog/',
   integrations: [mdx(), sitemap()],
 });
