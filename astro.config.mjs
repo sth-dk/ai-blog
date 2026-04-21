8104f00d0dbb9f20d5f0f39c24fe4fb666be698b
mengdeke <somethingdk@163.com>
2026-04-21 17:10:09 +0800

    fix: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://chuanglian.github.io',
+  site: 'https://sth-dk.github.io',
   base: '/ai-blog',
   integrations: [mdx(), sitemap()],
 });
